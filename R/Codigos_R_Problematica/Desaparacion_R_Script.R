1d6cfa17230e1c5bf9101f7abd4135218ba749ca
Yerson Antolinez <yerson2225611@correo.uis.edu.co>
2025-05-18 14:40:26 -0500

    Diagrama de barras

--- R/Codigos_R_Problematica/Desaparacion_R_Script.R
+++ R/Codigos_R_Problematica/Desaparacion_R_Script.R
@@ -1,1 +1,56 @@
-hola
+install.packages(c("arrow", "dplyr", "ggplot2","sf"))
+library(arrow)
+library(dplyr) #Util para eliminar columnas
+library(ggplot2) # para graficar
+library(sf)
+
+#Análisis del número de afectados entre los años 1985 a 2018 (homicidios, secuestros, desapariciones y reclutamiento). 
+#Gráfica : Histograma.
+
+# 1. Leer el archivo Parquet de Desapariciones
+df_hom <- read_parquet("C:/Datos_limpios/datos_filtrados_Desapariciones.parquet") 
+
+# 2. Filtrar el rango de años de interés y contar víctimas por año
+df_por_ano <- df_hom %>%
+  filter(yy_hecho >= 1985, yy_hecho <= 2018) %>%
+  group_by(yy_hecho) %>%
+  summarise(n_victimas = n(), .groups = "drop")
+
+
+# df_hom %>%:
+# Inicia una secuencia de comandos usando el operador pipe (%>%),
+# que permite leer y aplicar comandos en orden.
+
+# filter(yy_hecho >= 1985, yy_hecho <= 2018):
+# Filtra los registros del DataFrame df_hom.
+# 
+# - Condición: Solo mantiene las filas donde el año (yy_hecho) 
+#   está entre 1985 y 2018.
+# - Esto asegura que solo analizamos los datos del periodo de interés.
+
+# group_by(yy_hecho):
+# Agrupa los datos por año (yy_hecho).
+
+# summarise(n_victimas = n(), .groups = "drop"):
+# Para cada año (yy_hecho), cuenta el número de filas (número de víctimas).
+# 
+# - El resultado es una tabla con dos columnas:
+#   - yy_hecho: El año.
+#   - n_victimas: El número total de víctimas en ese año.
+# - .groups = "drop" evita que el resultado sea un objeto agrupado 
+
+
+# 3. Crear el histograma (barras por año)
+ggplot(df_por_ano, aes(x = yy_hecho, y = n_victimas)) +
+  geom_col(width = 0.8) +
+  scale_x_continuous(breaks = seq(1985, 2018, by = 1)) +
+  labs(
+    title    = "Número de víctimas de homicidio por año (1985–2018)",
+    x        = "Año",
+    y        = "Número de víctimas"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+    panel.grid.minor = element_blank()
+  )
